e15502bc7f062d745859211e0bcd88d6996b91e9
CMattia06 <9698278@itisrossi.vi.it>
2024-12-23 14:53:44 +0100

    Commenti del main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+//Avvio di un bot Telegram
+// La logica del bot è contenuta nella classe MyTelegramBot
+
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -5,10 +8,12 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) {
         try {
+            // Creazione di un'istanza di TelegramBotsApi utilizzando la sessione predefinita
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            // Registrazione del bot Telegram: 'MyTelegramBot' è la classe che definisce la logica del bot
             botsApi.registerBot(new MyTelegramBot());
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            System.out.println("Attenzione ERRORE durante la registrazione del bot");;
         }
 
     }
